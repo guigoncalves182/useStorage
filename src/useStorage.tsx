a57ddac74f22760f44747f54846129d89f15a13a
Guilherme Gonçalves <guigoncalves@msn.com>
2024-07-24 11:14:47 -0300

    useStorage

---
+++ src/useStorage.tsx
@@ -0,0 +1,61 @@
+type TStorage = "localStorage" | "sessionStorage";
+
+interface IUseStorage {
+  key: string;
+  storage?: TStorage;
+}
+
+export const useStorage = <T,>({
+  key,
+  storage = "localStorage",
+}: IUseStorage) => {
+  const setItem = (value: T): void => {
+    const stringValue = JSON.stringify(value);
+
+    switch (storage) {
+      case "sessionStorage":
+        sessionStorage.setItem(key, stringValue);
+        break;
+
+      default:
+        localStorage.setItem(key, stringValue);
+        break;
+    }
+  };
+
+  const getItem = (): T | null => {
+    let stringValue: string | null;
+
+    switch (storage) {
+      case "sessionStorage":
+        stringValue = sessionStorage.getItem(key);
+        break;
+
+      default:
+        stringValue = localStorage.getItem(key);
+        break;
+    }
+
+    let parsedValue: T | null = null;
+
+    if (stringValue) {
+      return (parsedValue = JSON.parse(stringValue));
+    }
+
+    return parsedValue;
+  };
+
+  const removeItem = (): void => {
+    switch (storage) {
+      case "sessionStorage":
+        if (key) sessionStorage.removeItem(key);
+        break;
+
+      default:
+        if (key) localStorage.removeItem(key);
+        break;
+    }
+  };
+
+  return { getItem, setItem, removeItem };
+};
